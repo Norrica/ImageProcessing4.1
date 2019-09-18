e70daedd46f81b5d7339181c7af01fac53b1c9f5
Norrica <a.kolpakov2010@mail.ru>
2019-09-18 23:16:48 +0500

    New branch apparently

--- 2.3.py
+++ 2.3.py
@@ -2,4 +2,12 @@ from skimage.io import imread, imshow, imsave
 from skimage import img_as_float, img_as_ubyte
 import numpy as np
 
-as
+img = imread("00.png")
+
+def align(img, g_coord):
+    row_g, col_g = g_coord
+    # считаем сдвиги каналов
+
+    # сдвигаем точку на зеленом канале
+    # на другие каналы
+    return #(row_b, col_b), (row_r, col_r)
